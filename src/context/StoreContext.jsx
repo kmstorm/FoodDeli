121917359b04ddd134118b6a8f2bf167ce862a5f
Your Name <your_email@example.com>
2024-10-26 14:55:55 +0700

    FE-update-food-display

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,13 +1,18 @@
 import { createContext } from "react";
-
+import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const contextValue = {
-        store: props.store,
-    };
+            store: props.store,
+            food_list: food_list,
+        };
 
-    return <StoreContext.Provider value={contextValue}>{props.children}</StoreContext.Provider>
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
 }
 
 export default StoreContextProvider;

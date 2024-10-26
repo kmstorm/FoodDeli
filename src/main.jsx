57fbe6432e9ad18d9147859943dcf552cce5aae8
Your Name <your_email@example.com>
2024-10-26 14:55:55 +0700

    FE-update-food-display

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'  
+import StoreContextProvider from './context/StoreContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider> {/* các con của app đều có thể sử dụng Context */}
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 )

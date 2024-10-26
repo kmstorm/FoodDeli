40abf15e06177e43e1400d846548fdc3f505d315
Đỗ Bảo <baba7cvk@gmail.com>
2024-10-26 14:55:55 +0700

    FE-update-food-display

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import Explore from '../../components/ExploreMenu/Explore'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 export const Home = () => {
-  const [category, setCategory] = useState("All");
+  const [category, setCategory] = useState("All");    
 
   return (
     <div>
         <Header />
-        <Explore category={category} setCategory={setCategory} />
+      <Explore category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }

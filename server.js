3a95216c5a155c4eeb8dc6959b17f2000e9103d2
Bui Quoc Bao <131287487+BaoHust2004@users.noreply.github.com>
2024-11-01 07:50:56 +0700

    Update api/user

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+import userRouter from "./routes/userRoutes"
 
 // app config
 const app = express()
@@ -9,6 +10,7 @@ const port = 3000
 // middleware
 app.use(express.json())
 app.use(cors())
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hello World")

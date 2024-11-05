614160bde47180094eb51494a0cd6684b0a711a9
Your Name <your_email@example.com>
2024-11-05 14:42:58 +0700

    FE-ItemDisplay-add-and-removeCart

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
-import { useState } from 'react'
+import { StoreContext } from '../../context/StoreContext'
+
 const FoodItem = ({ id, name, price, description, image }) => {
-     const [itemCount,setItemCount] = useState(0)
-  return (
-      <div className='food-item'>
-          <div className="food-item-img-container">
-              <img className='food-item-img' src={image} alt={name} />
-              {!itemCount
-                  ? <img className='food-item-add-btn' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="add" />
-                  : <div className='food-item-count'>
-                        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="remove" />
-                        <p>{itemCount}</p>
-                        <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="add" />
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext)
+    const [isAdding, setIsAdding] = useState(false)
+
+    const handleAddToCart = (itemId) => {
+        if (isAdding) return
+        setIsAdding(true)
+        addToCart(itemId)
+        setTimeout(() => {
+            setIsAdding(false)
+        }, 1000)
+    }
+
+    return (
+        <div className='food-item'>
+            <div className="food-item-img-container">
+                <img className='food-item-img' src={image} alt={name} />
+                {!cartItems[id]
+                    ? <img 
+                        className='food-item-add-btn' 
+                        onClick={() => handleAddToCart(id)} 
+                        src={assets.add_icon_white} 
+                        alt="add" 
+                        disabled={isAdding}
+                    />
+                    : <div className='food-item-count'>
+                        <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="remove" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="add" />
                     </div>}
-          </div>
-          <div className="food-item-info">
+            </div>
+            <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
-                  <img src={assets.rating_starts} alt="" />
+                    <img src={assets.rating_starts} alt="" />
                 </div>
                 <p className='food-item-description'>{description}</p>
                 <p className='food-item-price'>{price}</p>
-          </div>
-      </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default FoodItem

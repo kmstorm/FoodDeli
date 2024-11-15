3a560126aab4190fe15d911e1042d1b64af87276
Your Name <your_email@example.com>
2024-11-15 15:08:59 +0700

    FE-update-homepage-footer-and-add-appdownload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,16 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import Explore from '../../components/ExploreMenu/Explore'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 export const Home = () => {
   const [category, setCategory] = useState("All");    
 
   return (
     <div>
-        <Header />
+      <Header />
       <Explore category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   )
 }

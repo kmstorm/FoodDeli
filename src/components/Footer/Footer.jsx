f3a8de87a9d33e96fccfd355f085492a10eeb1e9
Đỗ Bảo <baba7cvk@gmail.com>
2024-11-15 07:19:19 +0700

    FE-homepage-footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Footer.css'
+export const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+          <div className="footer-content">
+                <div className="footer-content-left">
+                  
+                </div>
+                <div className="footer-content-center">
+                  
+                </div>
+                <div className="footer-content-right">
+                  
+                </div>
+          </div>
+    </div>
+  )
+}
+

7756dc08e0bfd51d95e1fd9fa2fb023d59096a29
Đỗ Bảo <baba7cvk@gmail.com>
2024-11-16 09:31:59 +0700

    FE-login-signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import { Footer } from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer"
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 export const App = () => {
+  const [showLogin, setShowLogin] = React.useState(false)
   return (
-    <div className="App">
-      <>
-        <Navbar />
+    <>
+      {showLogin? <LoginPopup setShowLogin={setShowLogin} />:<></>}
+      <div className="App">
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/placeorder" element={<PlaceOrder />} />
         </Routes>
         <Footer />
-      </>
-    </div>
+      </div>
+    </>
   )
 }
 

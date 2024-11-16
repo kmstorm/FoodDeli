f2e48efc15cf5d28b2d2fdd56ee0b632034e173a
Your Name <your_email@example.com>
2024-11-16 14:07:14 +0700

    FE-cart

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -27,11 +27,22 @@ const StoreContextProvider = (props) => {
         });
     }
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] === 0) continue;
+            let itemInfo = food_list.find((i) => i._id === item);
+            totalAmount += itemInfo.price * cartItems[item];
+        }
+        return totalAmount;
+    }
+
     const contextValue = {
         food_list: food_list,
         cartItems: cartItems,
         addToCart: addToCart,
         removeFromCart: removeFromCart,
+        getTotalCartAmount: getTotalCartAmount,
     };
 
     return (

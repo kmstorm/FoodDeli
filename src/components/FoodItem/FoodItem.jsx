f2c5911f9ee623bdad8ffadc4d95c11e34666de7
Your Name <your_email@example.com>
2024-11-18 09:20:51 +0700

    FE-axios-API

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
-    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext)
+    const { cartItems, addToCart, removeFromCart,url } = useContext(StoreContext)
     const [isAdding, setIsAdding] = useState(false)
 
     const handleAddToCart = (itemId) => {
@@ -19,7 +19,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
-                <img className='food-item-img' src={image} alt={name} />
+                <img className='food-item-img' src={url+"/images/"+image} alt={name} />
                 {!cartItems[id]
                     ? <img 
                         className='food-item-add-btn' 

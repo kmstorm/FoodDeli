db45066495d60225fe2e5a8fb0a0166f8386ee80
kmstorm <khaminhbao17082003@gmail.com>
2024-11-27 22:28:14 +0700

    move directory

---
+++ FrontEnd/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './FoodDisplay.css'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext.jsx'
+import FoodItem from '../FoodItem/FoodItem.jsx'
+const FoodDisplay = ({category}) => {
+    const { food_list } = useContext(StoreContext)
+  return (
+      <div className='food-display' id='food-display' >
+          <h2>Top dishes near you</h2>
+          <div className="food-display-list">
+            {food_list.map((item, index) => 
+              category === "All" || category === item.category ? (
+                <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image} />
+              ) : null
+            )}
+          </div>
+    </div>
+  )
+}
+
+export default FoodDisplay

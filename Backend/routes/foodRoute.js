a4ca3c0f8ed0fec9fd0c619b6c7aa19fa8d91bef
Bui Quoc Bao <131287487+BaoHust2004@users.noreply.github.com>
2024-12-14 13:32:47 +0700

    Update foodRoute.js:fix add route to fetch image on cloudinary

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import {addFood, listFood, removeFood} from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from 'multer';
+import path from 'path';
 
-const foodRouter = express.Router()
+const foodRouter = express.Router();
 
-// Image storage
+// Cấu hình Multer
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename:(req, file, callback) => {
-        return callback(null, `${Date.now()}${file.originalname}`)
-    }
-})
+  destination: "uploads/", // Thư mục lưu trữ tạm
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
 
-const upload = multer({storage: storage })
+// Các route API
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
-foodRouter.post("/add", upload.single("image"), addFood)
-foodRouter.get("/list",listFood)
-foodRouter.post("/remove",removeFood)
-
-export default foodRouter
+export default foodRouter;

4c66ce777f378f78781a2013f3e825336e708892
BaoHust2004 <buibaomoyu@gmail.com>
2024-12-16 00:12:27 +0700

    build BE-cart: add to Cart

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/userRoute.js"
 import {connectDB} from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import orderRouter from "./routes/orderRoute.js"
+import cartRouter from "./routes/cartRoute.js"
 
 // app config
 const app = express()
@@ -23,6 +24,7 @@ app.use("/api/food", foodRouter)
 app.use("/api/user",userRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/order",orderRouter)
+app.use("/api/cart",cartRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hello World")

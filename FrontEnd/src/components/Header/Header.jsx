4415329ee8d14bb642f111d61db624cf2ec75271
kmstorm <khaminhbao17082003@gmail.com>
2024-12-19 21:34:03 +0700

    fix: header font

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ export const Header = () => {
   return (
     <div className="header">
         <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <h2>Savor Every Bite</h2>
+            <p>Discover a world of flavors at your fingertips. Browse, order, and enjoy meals that turn every craving into a satisfying experience. Your next favorite dish is just a click away!</p>
             <button>View menu</button>
         </div>
 
